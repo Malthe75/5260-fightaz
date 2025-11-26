cfc276d27402255ed317655e9663dc1e97ec5a08
Malthe75 <Malthehf@gmail.com>
2025-11-26 19:49:37 +0100

    More playermovement stuff

--- Assets/Scripts/NEW PLAYER CONTROLLER/PlayerMovement.cs
+++ Assets/Scripts/NEW PLAYER CONTROLLER/PlayerMovement.cs
@@ -1,31 +1,74 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
+public struct MovementCommand
+{
+    public Vector2 velocity; // Units/sec
+    public bool doJump; // one-shot
+    public float jumpVelocity; // if doJump true
+}
 public class PlayerMovement : MonoBehaviour
 {
-    // Static list to keep track of all player instances
-    public static readonly List<PlayerMovement> players = new List<PlayerMovement>();
-    private void OnEnable() => players.Add(this);
-    private void OnDisable() => players.Remove(this);
-
-    private void FindEnemy()
-    {
-        enemy = All.FirstOrDefault(players => p != this);
-        if(enemy == null) Debug.Log("Enemy not found yet: Try again later.");
-    }
+    private MovementCommand currentCommand;
+    private bool hasCommand;
 
     // Other stuff
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform body;
+    private Collider2D pushbox;
     private PlayerMovement enemy;
 
 
+    public float minX = -5f;
+    public float maxX = 5f;
+    public float minY = 0f;
 
+
+    public void SetMovementCommand(MovementCommand command)
+    {
+        currentCommand = command;
+        hasCommand = true;
+    }
+    void FixedUpdate()
+    {
+        float dt = Time.fixedDeltaTime;
+
+        if (hasCommand)
+        {
+            
+            // Perform 
+
+            // Move
+            Vector2 movement = currentCommand.velocity * dt;
+            Vector2 allowedMove = PushboxCalculator(movement);
+            Vector2 clampedPos = ClampedMovement(allowedMove);
+            rb.MovePosition(clampedPos);
+
+            // Jump
+            if (currentCommand.doJump)
+            {
+                Vector2 jumpMovement = new Vector2(0f, currentCommand.jumpVelocity) * dt;
+                Vector2 allowedJumpMove = PushboxCalculator(jumpMovement);
+                Vector2 clampedJumpPos = ClampedMovement(allowedJumpMove);
+                rb.MovePosition(clampedJumpPos);
+            }
+                
+            hasCommand = false;
+        }
+
+
+    }
+
+
+    // This class is 
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        pushbox = body.GetComponent<Collider2D>();
+    }
     private void Start()
     {
-        FindEnemy();
-        rb = GetComponent<Rigidbody2D>();
+        SetEnemy();
     }
 
     public void Move(float direction, float speed)
@@ -35,7 +78,7 @@ public class PlayerMovement : MonoBehaviour
 
         Vector2 allowedMove = PushboxCalculator(desiredMove);
 
-        player.rb.MovePosition(allowedMove);
+        rb.MovePosition(allowedMove);
     }
 
     public void Jump(float xVelocity, float yVelocity ,float jumpForce)
@@ -44,7 +87,7 @@ public class PlayerMovement : MonoBehaviour
 
         Vector2 allowedMove = PushboxCalculator(movement);
 
-        player.rb.MovePosition(allowedMove);
+        rb.MovePosition(allowedMove);
     }
 
     public void Fall()
@@ -57,10 +100,10 @@ public class PlayerMovement : MonoBehaviour
         // Push player logic
     }
 
-    public Vector2 PushboxCalculator(Vector2 movement)
-    {
-        Vector2 rayOrigin = (Vector2)body.transform.position;
-    }
+    // public Vector2 PushboxCalculator(Vector2 movement)
+    // {
+    //     Vector2 rayOrigin = (Vector2)body.transform.position;
+    // }
 
 
 public Vector2 PushboxCalculator(Vector2 desiredMove)
@@ -80,32 +123,58 @@ public Vector2 PushboxCalculator(Vector2 desiredMove)
         Debug.DrawRay(rayOrigin, rayDirection * rayLength, Color.red);
 
         // Raycasthits
-        RaycastHit2D[] hits = Physics2D.RaycastAll(rayOrigin, rayDirection, rayLength, playerLayerMask);
+        //RaycastHit2D[] hits = Physics2D.RaycastAll(rayOrigin, rayDirection, rayLength, playerLayerMask);
 
         // Foreach loop to iterate all raycasts and only getting the ones that hit the enemy player.
-        foreach (RaycastHit2D hit in hits)
-        {
-            if (hit.collider != null && hit.collider != pushbox)
-            {
-                PushPlayer();
-                bool blockedRight = rayDirection.x > 0;
-                bool blockedLeft = rayDirection.x < 0;
+        // foreach (RaycastHit2D hit in hits)
+        // {
+        //     if (hit.collider != null && hit.collider != pushbox)
+        //     {
+        //         //PushPlayer();
+        //         bool blockedRight = rayDirection.x > 0;
+        //         bool blockedLeft = rayDirection.x < 0;
 
-                if (blockedRight && desiredMove.x > 0f)
-                    desiredMove.x = 0f;          // stop rightward motion
-                else if (blockedLeft && desiredMove.x < 0f)
-                    desiredMove.x = 0f;          // stop leftward motion
+        //         if (blockedRight && desiredMove.x > 0f)
+        //             desiredMove.x = 0f;          // stop rightward motion
+        //         else if (blockedLeft && desiredMove.x < 0f)
+        //             desiredMove.x = 0f;          // stop leftward motion
 
-                Debug.DrawRay(hit.point, Vector2.up * 0.5f, Color.green);
+        //         Debug.DrawRay(hit.point, Vector2.up * 0.5f, Color.green);
                 
-            }   
-        }
+        //     }   
+        // }
         return desiredMove;
     }
-    public Vector2 ClampedMovement()
+    public Vector2 ClampedMovement(Vector2 desiredMove)
     {
-        return null;
+        Vector2 nextPos = rb.position + desiredMove;
+
+        nextPos.x = Mathf.Clamp(nextPos.x, minX, maxX);
+        nextPos.y = Mathf.Max(nextPos.y, minY);
+        return nextPos;
     }
 
+
+
     
+
+
+
+
+
+
+
+    #region MIGHT BE MOVED OUT OF HERE LATER
+    private void SetEnemy()
+    {
+        if(transform.CompareTag("Player1"))
+        {
+            enemy = GameObject.FindGameObjectWithTag("Player2").GetComponent<PlayerMovement>();
+        }
+        else
+        {
+            enemy = GameObject.FindGameObjectWithTag("Player1").GetComponent<PlayerMovement>();
+        }
+    }
+    #endregion
 }
